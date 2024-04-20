a62b88774d7306d27b175178e8bbc820364be15d
bibek190 <bibek9818917036@gmail.com>
2024-04-21 02:05:07 +1000

    working on layout

--- App.js
+++ App.js
@@ -2,16 +2,25 @@ import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  function goalInputHandler(enteredText) {
+    console.log(enteredText);
+  }
+  const addGoalHandler = () => {};
+
   return (
     <View style={styles.appContainer}>
       {/* What are listed */}
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your Course Goal !" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your Course Goal !"
+          onChangeText={goalInputHandler}
+        />
         <Button title="Add Goal" />
       </View>
       {/*What are written  */}
-      <View>
-        <Text>List of Goals</Text>
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals...</Text>
       </View>
     </View>
   );
@@ -19,17 +28,28 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
     padding: 50,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItem: "center",
+    marginTop: 20,
+    paddingBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
-    borderColor: "#cccccc",
-    width: "80%",
-    padding: 10,
+    borderColor: "blue",
+    width: " 70%",
+    height: 60,
+    padding: 8,
     marginRight: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
